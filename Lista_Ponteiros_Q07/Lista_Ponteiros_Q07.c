b11387df7fd1b2e5f3967ffec56a8fc099cf3dc4
matheusslr <54174040+matheusslr@users.noreply.github.com>
2019-08-17 10:23:56 -0300

    Update Lista_Ponteiros_Q07.c

--- Lista_Ponteiros_Q07/Lista_Ponteiros_Q07.c
+++ Lista_Ponteiros_Q07/Lista_Ponteiros_Q07.c
@@ -2,7 +2,15 @@
 
 int main(){
     int pulo[5]={1,2,3,4,5};
-
-    printf("%d",*(pulo+2));     //R:a expressao que corresponde o terceiro elemento do vetor é *(pulo+2)
+    
+    /*  alternativas
+        
+        (pulo + 2);
+        *(pulo + 4);
+        pulo + 4;
+        pulo + 2;        */
+    
+    
+    printf("%d",*(pulo+2));     //R:a expressao que corresponde o terceiro elemento do vetor Ã© *(pulo+2)
 
 }
